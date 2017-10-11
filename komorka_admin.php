a427393602c36982b609b87167d72ffc9f805568
Amanda <amaenta@gmail.com>
2017-10-11 13:19:56 +0200

    confirming requests by admin

--- komorka_admin.php
+++ komorka_admin.php
@@ -31,19 +31,19 @@ foreach (range(12, 16) as $hour) {
 
 if ($rezultat = $polaczenie->query(
     sprintf
-    ("SELECT * FROM events AS e LEFT JOIN uzytkownicy AS u ON e.user_id = u.id WHERE `date`='%s'",
+    ("SELECT *, e.id AS event_id FROM events AS e LEFT JOIN uzytkownicy AS u ON e.user_id = u.id WHERE `date`='%s'",
         mysqli_real_escape_string($polaczenie, $date)))) {
     $events = $rezultat->fetch_all(MYSQLI_ASSOC);
     foreach ($events as $event) {
         if ($event['confirmed'] == 1 && ($event['nazwa'])) {
             list($name, $surname) = explode(" ", $event['nazwa'], 2);
-            $event['nazwa'] = $name[0] . ". " . $surname;
+            $event['shortname'] = $name[0] . ". " . $surname;
             $event['button_class'] = "switch";
             $event['button_status'] = "disabled";
         }
         if ($event['confirmed'] == 0 && ($event['nazwa'])) {
             list($name, $surname) = explode(" ", $event['nazwa'], 2);
-            $event['nazwa'] = $name[0] . ". " . $surname;
+            $event['shortname'] = $name[0] . ". " . $surname;
             $event['button_class'] = "waiting";
             $event['button_status'] = "disabled";
             $event['add_icon'] = "<i class=\"fa fa-plus-square\"></i>";
@@ -59,7 +59,14 @@ if ($rezultat = $polaczenie->query(
 <html>
 <head>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-</head>
+    <style>
+        .button-icon {
+            color: #313e39;
+            cursor: pointer;
+            width: 16px;
+            height: 14px;
+        }
+    </style>
 </head>
 <body>
 <div class="row">
@@ -79,18 +86,18 @@ if ($rezultat = $polaczenie->query(
                                 data-date="<?php echo $date ?>"
                             <?php echo $event['button_status'] ?>
                                 onclick="setColor(this)">
-                            <?php echo $event['nazwa']; ?>
+                            <?php echo $event['shortname']; ?>
                         </button>
                     </div>
-                    <div class="col-sm-1">
-                        <span>
-                        <button type="submit" class="btn btn-link btn-sm" style="color: #2CC990; cursor: pointer">
+                    <div class="col-sm-1" style="margin-left: -10px">
+                        <button type="submit" class="btn btn-link btn-sm  float-left add-button button-icon"
+                                data-id="<?php echo $event['event_id'] ?>">
                             <?php echo $event['add_icon'] ?>
                         </button>
-                        <button type="submit" class="btn btn-link btn-sm" style="color: #2CC990; cursor: pointer">
+                        <button type="submit" class="btn btn-link btn-sm  float-left remove-button button-icon"
+                                data-id="<?php echo $event['event_id'] ?>">
                             <?php echo $event['remove_icon'] ?>
                         </button>
-                        </span>
                     </div>
                 <?php endforeach ?>
 
@@ -101,7 +108,8 @@ if ($rezultat = $polaczenie->query(
 <div id="footer" class="modal-footer">
     <button id="send" type="button" class="btn float-right">ZAPISZ</button>
 </div>
-<div id="koncowy">
+<div id="checker">
+
 </div>
 <script>
     function setColor(e) {
@@ -112,25 +120,47 @@ if ($rezultat = $polaczenie->query(
     }
 </script>
 <script>
-    $('#send').click(function () {
-        var ms = [];
-        $('.activated').each(function (index, element) {
-            var $element = $(element);
-            ms.push({
-                date: $element.data("date"),
-                hour: $element.data("hour"),
-                column: $element.data("column")
-            });
-        });
-        console.log(ms);
+    $('.remove-button').click(function () {
+
+        var data = $(this).data();
+        console.log(data);
         $.ajax({
             type: "POST",
-            url: '/askfor.php',
-            data: {
-                events: ms
-            },
+            url: '/remRequest.php',
+            data: data,
             success: function () {
-                document.getElementById("modal").innerHTML = "Wysłałeś prośbę, dziękujemy!";
+                $.ajax({
+                    type: "GET",
+                    url: '/komorka_admin.php',
+                    data: {
+                        date: "<?php echo $date ?>"
+                    },
+                    success: function (response) {
+                        $('#exampleModal .modal-body').html(response);
+                    }
+                });
+            }
+        });
+    });
+    $('.add-button').click(function () {
+
+        var data = $(this).data();
+        console.log(data);
+        $.ajax({
+            type: "POST",
+            url: '/addRequest.php',
+            data: data,
+            success: function () {
+                $.ajax({
+                    type: "GET",
+                    url: '/komorka_admin.php',
+                    data: {
+                        date: "<?php echo $date ?>"
+                    },
+                    success: function (response) {
+                        $('#exampleModal .modal-body').html(response);
+                    }
+                });
             }
         });
     })
---
+++ remRequest.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['zalogowany'])) {
+
+    header('Location: index.php');
+    exit();
+}
+$tb = $_POST;
+$updating_record = $tb['id'];
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=wozek;charset=utf8', 'root', 'root');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->prepare("DELETE FROM `events` WHERE `id`=$updating_record");
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
+}
+
+?>
